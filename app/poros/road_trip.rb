09f1a5d87f5d347bcd0759184567f3b7523187d6
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-08-11 14:22:55 +0200

    Adds edge cases for road trip

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -9,13 +9,33 @@ class RoadTrip
     @origin =      trip_data[:origin]
     @destination = trip_data[:destination]
     @travel_time = time_format(trip_data)
-    @temperature = trip_data[:weather_eta].temperature
-    @conditions =  trip_data[:weather_eta].conditions
+    @temperature = arrival_temp(trip_data)
+    @conditions =  arrival_cond(trip_data)
   end
 
   def time_format(trip_data)
-    hour = trip_data[:travel_time].split(':')[0]
-    min = trip_data[:travel_time].split(':')[1]
-    "#{hour} hours, #{min} minutes"
+    if trip_data[:travel_time].nil?
+      "Impossible"
+    else
+      hour = trip_data[:travel_time].split(':')[0]
+      min = trip_data[:travel_time].split(':')[1]
+      "#{hour} hours, #{min} minutes"
+    end
+  end
+
+  def arrival_temp(trip_data)
+    if trip_data[:travel_time].nil?
+      trip_data[:weather_eta] = nil
+    else
+      trip_data[:weather_eta].temperature
+    end
+  end
+
+  def arrival_cond(trip_data)
+    if trip_data[:travel_time].nil?
+      trip_data[:weather_eta] = nil
+    else
+      trip_data[:weather_eta].conditions
+    end
   end
 end
--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -30,5 +30,50 @@ RSpec.describe 'Roadtrip API' do
       expect(weather_at[:temperature]).to be_an(Float)
       expect(weather_at[:conditions]).to be_an(String)
     end
+
+    it 'Returns impossible response', :vcr do
+      post '/api/v1/road_trip', params:
+      {
+        origin: 'Brooklyn, NY',
+        destination: 'Amsterdam, NL',
+        api_key: "b5ef74dc2166f263c6c1"
+      }
+
+      expect(response).to have_http_status(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to be_a(Hash)
+      expect(data[:data]).to be_a(Hash)
+      expect(data[:data][:id]).to eq(nil)
+      expect(data[:data][:type]).to eq('roadtrip')
+      expect(data[:data][:attributes]).to be_a(Hash)
+
+      road_trip = data[:data][:attributes]
+      expect(road_trip[:start_city]).to be_an(String)
+      expect(road_trip[:end_city]).to be_an(String)
+      expect(road_trip[:travel_time]).to eq("Impossible")
+
+      expect(road_trip[:weather_at_eta]).to be_an(Hash)
+
+      weather_at = road_trip[:weather_at_eta]
+      expect(weather_at[:temperature]).to eq(nil)
+      expect(weather_at[:conditions]).to eq(nil)
+    end
+
+    it 'Returns an error message', :vcr do
+      post '/api/v1/road_trip', params:
+      {
+        origin: 'Brooklyn, NY',
+        destination: 'Miami, FL',
+        api_key: " "
+      }
+
+      expect(response).to have_http_status 403
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body).to eq({error: 'The Api Key submitted with this request is invalid.'})
+    end
   end
 end
