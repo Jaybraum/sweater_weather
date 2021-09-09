b1af1a6f6c0439e6ed144da764bff741cc483394
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-09-09 21:43:34 +0200

    Authenticates user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       render json: UsersSerializer.details(user), status: 200
     else
       render json: {error: 'Invalid Credentials'}, status: 401
